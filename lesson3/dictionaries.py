811cb8c4f831de2c472bd816dee4bfa206b86685
Helen Salazar <41647323+Helen-2000@users.noreply.github.com>
2020-08-25 17:06:36 -0500

    Add files via upload

---
+++ lesson3/dictionaries.py
@@ -0,0 +1,29 @@
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions of people.
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+
+population = {'Shanghai': 17.8, 'Istanbul': 13.3, 'Karachi': 13.0, 'Mumbai': 12.5}
+
+
+# Try your hand at working with nested dictionaries. Add another entry,
+# 'is_noble_gas,' to each dictionary in the elements dictionary.
+
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+
+# todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
+# hint: helium is a noble gas, hydrogen isn't
+
+elements['helium']['is_noble_gas'] = True
+elements['hydrogen']['is_noble_gas'] = False
+print(elements)
